d60543cd5f4b36a992c12f64015c9cb51dd46ff5
James Harrison <jamesebharrison@hotmail.com>
2022-01-03 16:35:55 +0000

    spell checked line 94 and 95

--- script.js
+++ script.js
@@ -91,8 +91,8 @@ const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandandTextElement = document.querySelector('[data-current-operand]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const Calculator = new calculator(previousOperandandTextElement, currentOperandandTextElement)
 
